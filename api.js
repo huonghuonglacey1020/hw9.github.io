bbe2f45232a79a3e1df57aada8daf4575dc56dec
huongnguyen <huonghuonglacey1020@gmail.com>
2020-03-24 16:49:02 -0700

    yah profile generator

---
+++ api.js
@@ -0,0 +1,90 @@
+const axios = require ('axios');
+const inquirer = require('inquirer')
+const pdf = require("html-pdf");
+const fs = require('fs');
+const util = require("util")
+const generateHTML = require("./generateHTML");
+const writeFileAsync = util.promisify(fs.writeFile);
+
+
+
+const question = [
+    
+    {
+        type: "input",
+        name: "username",
+        message: "Enter your GitHub Username",
+    },
+    {
+        type: "list",
+        choices:["pink", "red", "blue", "green"],
+        message: "what color do you like?",
+        name: "color",
+    },
+];
+require('dotenv').config();
+
+
+function getUser() {
+        return new Promise(function(resolve, reject){
+            inquirer.prompt(question).then(async function(answers){
+                const {username, color} = answers;
+                const url = `https://api.github.com/users/${username}?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}`;
+                try {
+                    const response = await axios.get(url);
+                    const stars = await getStars(username);
+                    resolve({
+                        ...response.data,
+                        color,
+                        stars,
+
+                    });
+
+                } catch (error){
+                    reject(error)
+                };
+                
+            });
+        });
+    };
+
+function getStars(username) {
+        const url1 = `https://api.github.com/users/${username}/starred`;
+        return axios.get(url1).then(function(res) {
+            const repo = res.data.length;
+            return repo;
+        });
+}
+        
+        
+        
+        
+init();
+        
+async function init(repo) {
+    try{
+        const data = await getUser();
+        const html = generateHTML(data);
+        await writeFileAsync("./profile.html", html)
+        pdf.create(html).toFile("./profile.pdf", function(err, res){
+            console.log(res.profile)
+                
+
+        });
+    } catch (error) {
+        console.log(error)
+    }
+};
+getStars();
+
+
+
+
+
+
+
+
+
+
+
+
